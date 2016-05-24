2eef42499648e824e5b2d8f1838914a94b2f0196
Anthony Pellicani <anthony.pellicani@gmail.com>
2016-05-24 21:04:17 +0200

    styles changes

--- app/src/main/java/it/uniba/di/ivu/sms16/gruppo3/fasterfood/HomeActivity.java
+++ app/src/main/java/it/uniba/di/ivu/sms16/gruppo3/fasterfood/HomeActivity.java
@@ -3,17 +3,20 @@ package it.uniba.di.ivu.sms16.gruppo3.fasterfood;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     private Toolbar myToolbar;
     private DrawerLayout mDrawerLayout;
@@ -27,6 +30,8 @@ public class HomeActivity extends AppCompatActivity {
         setupToolbar();
         setupNavigationDrawer();
         setupFragment();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
     }
 
     private void setupFragment(){
@@ -35,7 +40,6 @@ public class HomeActivity extends AppCompatActivity {
 
     private void setupToolbar(){
         myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-
         myToolbar.setLogo(R.mipmap.ic_launcher);
         myToolbar.setTitle(R.string.app_name);
         myToolbar.setTitleTextColor(Color.WHITE);
@@ -49,5 +53,22 @@ public class HomeActivity extends AppCompatActivity {
         mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, myToolbar, R.string.open_drawer, R.string.closed_drawer);
         mDrawerToggle.syncState();
 
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        return false;
     }
 }
