cea36d75352889df92d8050480a82a162b11dc3e
Anthony Pellicani <anthony.pellicani@gmail.com>
2016-05-24 16:44:10 +0200

    BETA: Aggiunto drawer menu

--- app/src/main/java/it/uniba/di/ivu/sms16/gruppo3/fasterfood/HomeActivity.java
+++ app/src/main/java/it/uniba/di/ivu/sms16/gruppo3/fasterfood/HomeActivity.java
@@ -1,16 +1,11 @@
 package it.uniba.di.ivu.sms16.gruppo3.fasterfood;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.graphics.Color;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -28,21 +23,30 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-
         setupToolbar();
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, myToolbar, R.string.open_drawer, R.string.closed_drawer){
+        setupNavigationDrawer();
+    }
 
-        };
+    private void setupToolbar(){
+        myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+
+        myToolbar.setLogo(R.mipmap.ic_launcher);
+        myToolbar.setTitle(R.string.app_name);
+        myToolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(myToolbar);
+    }
+
+    private void setupNavigationDrawer(){
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, myToolbar, R.string.open_drawer, R.string.closed_drawer);
         mDrawerToggle.syncState();
         //popolazione listview
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
         ArrayList<SettingsElement> settingsList = new ArrayList<>(); //lista delle impostazioni che la listview visualizzerà
 
-            settingsList.add(new SettingsElement(R.drawable.ic_silverware_variant, getResources().getString(R.string.home_settings)));
-            settingsList.add(new SettingsElement(R.drawable.ic_silverware_variant, getResources().getString(R.string.orders_settings)));
-            settingsList.add(new SettingsElement(R.drawable.ic_silverware_variant, getResources().getString(R.string.locals_settings)));
+        settingsList.add(new SettingsElement(R.drawable.ic_silverware_variant, getResources().getString(R.string.home_settings)));
+        settingsList.add(new SettingsElement(R.drawable.ic_silverware_variant, getResources().getString(R.string.orders_settings)));
+        settingsList.add(new SettingsElement(R.drawable.ic_silverware_variant, getResources().getString(R.string.locals_settings)));
 
 
         /*******************************************
@@ -63,27 +67,6 @@ public class HomeActivity extends AppCompatActivity {
         int[] to={R.id.settingImage,R.id.settingName};//agli id delle view
 
         //costruzione dell adapter
-        SimpleAdapter adapter=new SimpleAdapter(
-                getApplicationContext(),
-                data,//sorgente dati
-                R.layout.custom_row, //layout contenente gli id di "to"
-                from,
-                to);
-
         mDrawerList.setAdapter(new SimpleAdapter(getApplicationContext(), data, R.layout.custom_row,from, to));
-    }
-
-    private void setupToolbar(){
-        myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-
-        myToolbar.setLogo(R.mipmap.ic_launcher);
-        myToolbar.setTitle(R.string.app_name);
-        myToolbar.setTitleTextColor(Color.WHITE);
-        setSupportActionBar(myToolbar);
-    }
-
-    public void funzione(View view){
-        Intent i = new Intent(this, LoginActivity.class);
-        startActivity(i);
     }
 }
