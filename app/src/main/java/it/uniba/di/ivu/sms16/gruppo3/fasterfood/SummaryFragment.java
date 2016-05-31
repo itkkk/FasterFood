bc686a00895f38f4c0cdfeb7a0072370d97d832f
a_starrr <spa.angelo@gmail.com>
2016-05-31 10:04:10 +0200

    Aggiunta finestra riassuntuvia: SummaryFragment + layout

--- app/src/main/java/it/uniba/di/ivu/sms16/gruppo3/fasterfood/SummaryFragment.java
+++ app/src/main/java/it/uniba/di/ivu/sms16/gruppo3/fasterfood/SummaryFragment.java
@@ -6,10 +6,13 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -37,18 +40,43 @@ public class SummaryFragment extends Fragment {
         RecyclerView.Adapter adapterSummaryRV = new SummaryRVAdapter(daCanc, getActivity().getApplicationContext());
         summaryRV.setAdapter(adapterSummaryRV);
 
-        final Switch switchSeats = (Switch) getView().findViewById(R.id.switchSeats);
+        Switch switchSeats = (Switch) getView().findViewById(R.id.switchSeats);
         final LinearLayout layoutSeats = (LinearLayout) getView().findViewById(R.id.layoutSeats);
+        final EditText editTextSeats = (EditText) getView().findViewById(R.id.editTextSeats);
+        final TextView zero_seats = (TextView) getView().findViewById(R.id.txtViewMessage);
+
 
         switchSeats.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                                                   @Override
-                                                   public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                                                       if (layoutSeats.getVisibility() == View.GONE)
-                                                           layoutSeats.setVisibility(View.VISIBLE);
-                                                       else
-                                                           layoutSeats.setVisibility(View.GONE);
-                                                   }
-                                               }
+           @Override
+           public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+               if (isChecked) {
+                   layoutSeats.setVisibility(View.VISIBLE);
+                   editTextSeats.addTextChangedListener(new TextWatcher() {
+                       @Override
+                       public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                       }
+
+                       @Override
+                       public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                       }
+
+                       @Override
+                       public void afterTextChanged(Editable s) {
+                           if (s.toString().isEmpty() || Integer.valueOf(s.toString()) < 1)
+                               zero_seats.setVisibility(View.VISIBLE);
+                           else
+                               zero_seats.setVisibility(View.GONE);
+                       }
+                   });
+               } else {
+                   layoutSeats.setVisibility(View.GONE);
+                   zero_seats.setVisibility(View.GONE);
+               }
+
+           }
+       }
         );
 
         TextView avaiableSeats = (TextView) getView().findViewById(R.id.avaiableSeats);
