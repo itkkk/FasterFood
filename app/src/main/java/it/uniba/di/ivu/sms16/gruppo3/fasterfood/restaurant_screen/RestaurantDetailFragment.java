80bfa5b6acafd7d4d4b42f5d1e40bdfe99681762
spaan <spa.angelo@gmail.com>
2016-07-21 15:43:26 +0200

    Orari finiti

--- app/src/main/java/it/uniba/di/ivu/sms16/gruppo3/fasterfood/restaurant_screen/RestaurantDetailFragment.java
+++ app/src/main/java/it/uniba/di/ivu/sms16/gruppo3/fasterfood/restaurant_screen/RestaurantDetailFragment.java
@@ -7,29 +7,25 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.FrameLayout;
+
 import android.widget.RatingBar;
 import android.widget.ScrollView;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
+
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.vision.barcode.Barcode;
+
 
 import java.io.IOException;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.List;
 
 import it.uniba.di.ivu.sms16.gruppo3.fasterfood.HomeActivity;
 import it.uniba.di.ivu.sms16.gruppo3.fasterfood.R;
@@ -161,7 +157,7 @@ public class RestaurantDetailFragment extends Fragment{
 
             // Fine Geodecoding
             if (address == null){
-                //Toast.makeText(getActivity(), "Rete non dispo", Toast.LENGTH_LONG).show();
+
             }
             else {
                 restaurantLatLng = new LatLng(address.getLatitude(), address.getLongitude());
