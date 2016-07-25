729a444e402f8a673aff68b79918098f8d71c2fd
Francesco Mondelli <f.mondelli93@gmail.com>
2016-07-25 21:06:08 +0200

    Modifiche filter persistenza

--- app/src/main/java/it/uniba/di/ivu/sms16/gruppo3/fasterfood/orders_screen/FilterOrdersFragment.java
+++ app/src/main/java/it/uniba/di/ivu/sms16/gruppo3/fasterfood/orders_screen/FilterOrdersFragment.java
@@ -2,8 +2,11 @@ package it.uniba.di.ivu.sms16.gruppo3.fasterfood.orders_screen;
 
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,14 +30,29 @@ public class FilterOrdersFragment extends Fragment {
     //back to order frag
     private Button save_btn;
     private RadioGroup dinamicRG;
+    private RadioButton dinamicRB[];
+    private RadioButton rad1no;
+    private RadioButton rad1open;
+    private RadioButton rad1closed;
+    private RadioButton rad2no;
     View layout;
+    SharedPreferences prefs;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
          layout=inflater.inflate(R.layout.fragment_filter_orders, container, false);
 
+        //INIZIALIZZAZIONE ITEM BASE
+        //recupero file preferences specifico per i settings
+        prefs=this.getActivity().getSharedPreferences(getActivity().getResources().getString(R.string.shared_pref_filter_name)
+                , Context.MODE_PRIVATE);
+
         save_btn=(Button)layout.findViewById(R.id.filter_btn);
         dinamicRG=(RadioGroup) layout.findViewById(R.id.radio2);
+        rad1no=(RadioButton) layout.findViewById(R.id.radio1no);
+        rad1open=(RadioButton) layout.findViewById(R.id.radio1open);
+        rad1closed=(RadioButton) layout.findViewById(R.id.radio1closed);
+        rad2no=(RadioButton) layout.findViewById(R.id.radio2no);
 
         //dati dal db
         chainList = ScambiaDati.getChainList();
@@ -46,9 +64,17 @@ public class FilterOrdersFragment extends Fragment {
         chain_number=(chainList.getChains().size());
         addRadioBtn(chain_number);
 
-                save_btn.setOnClickListener(new View.OnClickListener() {
+        setChecks();
+
+        save_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putString(getActivity().getResources().getString(R.string.shared_pref_first_check),
+                        findCheck1());
+                editor.putString(getActivity().getResources().getString(R.string.shared_pref_second_check),
+                        findCheck2(chain_number));
+                editor.apply();
                 set_orderFrag();
             }
         });
@@ -69,11 +95,68 @@ public class FilterOrdersFragment extends Fragment {
     }
 
     private void addRadioBtn(int number) {
-        RadioButton dinamicRB[]=new RadioButton[number];
+        dinamicRB = new RadioButton[number];
         for (int i = 0; i < number; i++) {
             dinamicRB[i] = new RadioButton(getActivity());
             dinamicRB[i].setText(filterArray[i]);
             dinamicRG.addView(dinamicRB[i]);
         }
+    }
+
+    private void setChecks(){
+        String first;
+        String second;
+
+        first=prefs.getString(getActivity().getResources().getString(R.string.shared_pref_first_check),
+                getActivity().getResources().getString(R.string.no_filter));
+        second=prefs.getString(getActivity().getResources().getString(R.string.shared_pref_second_check),
+                getActivity().getResources().getString(R.string.no_filter));
+
+        //prima ricerca
+        if(rad1no.getText().equals(first)){
+            rad1no.setChecked(true);
+        }else if(rad1open.getText().equals(first)){
+            rad1open.setChecked(true);
+        }else{
+            rad1closed.setChecked(true);
+        }
+
+        //seconda ricerca
+        if(rad2no.getText().equals(second)){
+            rad2no.setChecked(true);
+        }else{
+            int i = 0;
+            while(dinamicRB[i].getText().equals(second)){
+                i++;
+            }
+            dinamicRB[i].setChecked(true);
+        }
+    }
+
+    private String findCheck1(){
+        String first_check;
+
+        if(rad1no.isChecked()){
+            first_check=rad1no.getText().toString();
+        }else if(rad1open.isChecked()){
+            first_check=rad1open.getText().toString();
+        }else{
+            first_check=rad1closed.getText().toString();
+        }
+        return first_check;
+    }
+
+    private String findCheck2(int number){
+        String second_check;
+        if(rad2no.isChecked()){
+            second_check=rad2no.getText().toString();
+        }else {
+            int i = 0;
+            while(!dinamicRB[i].isChecked()){
+                i++;
+            }
+            second_check=dinamicRB[i].getText().toString();
+        }
+        return second_check;
     }
 }
