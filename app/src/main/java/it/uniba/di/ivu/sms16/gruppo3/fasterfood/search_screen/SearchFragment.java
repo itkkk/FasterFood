9b9e9defbaadb5ad7d5dac5480d380718c08de7e
spaan <spa.angelo@gmail.com>
2016-07-25 10:09:26 +0200

    :D

--- app/src/main/java/it/uniba/di/ivu/sms16/gruppo3/fasterfood/search_screen/SearchFragment.java
+++ app/src/main/java/it/uniba/di/ivu/sms16/gruppo3/fasterfood/search_screen/SearchFragment.java
@@ -148,7 +148,6 @@ public class SearchFragment extends Fragment{
 
         // Spinner
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 filteredLocalsList.clear();
