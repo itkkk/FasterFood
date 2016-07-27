b3e1f4acfc7a63a38a00af69a3d71fa394010e2a
Corrado <tatinfo94@gmail.com>
2016-07-28 01:13:04 +0200

    Pulizia codice

--- app/src/main/java/it/uniba/di/ivu/sms16/gruppo3/fasterfood/notification_screen/ReviewActivity.java
+++ app/src/main/java/it/uniba/di/ivu/sms16/gruppo3/fasterfood/notification_screen/ReviewActivity.java
@@ -3,24 +3,23 @@ package it.uniba.di.ivu.sms16.gruppo3.fasterfood.notification_screen;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.provider.ContactsContract;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.File;
 
+import it.uniba.di.ivu.sms16.gruppo3.fasterfood.AppConfiguration;
 import it.uniba.di.ivu.sms16.gruppo3.fasterfood.R;
 import it.uniba.di.ivu.sms16.gruppo3.fasterfood.db.ScambiaDati;
 
 public class ReviewActivity extends AppCompatActivity {
+    private Intent mReviewInt;
     private RatingBar mCurrentRating;
     private Button mBtnReview;
     private TextView mTxtLocalMenu;
@@ -32,28 +31,10 @@ public class ReviewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
 
-        final Intent reviewInt = getIntent();
+        mReviewInt = getIntent();
 
-        mBtnReview = (Button) findViewById(R.id.buttonReview);
-        mTxtLocalMenu = (TextView) findViewById(R.id.reviewNameLocal);
-        mCurrentRating = (RatingBar) findViewById(R.id.ratingReview);
-        mLocalImage = (ImageView) findViewById(R.id.reviewMenuImage);
-
-
-        mTxtLocalMenu.setText(reviewInt.getStringExtra("LocalName"));
-
-        // CONTROLLO CHAIN PER OTTENERE IL LOGO
-        if(mTxtLocalMenu.getText().toString().contains(getResources().getString(R.string.mcdonalds))){
-            mLogo = ScambiaDati.getLogo(0);
-        }
-        else if(mTxtLocalMenu.getText().toString().contains(getResources().getString(R.string.burgerking))){
-            mLogo = ScambiaDati.getLogo(1);
-        }
-        else if(mTxtLocalMenu.getText().toString().contains(getResources().getString(R.string.baciodilatte))){
-            mLogo = ScambiaDati.getLogo(2);
-        }
-        Bitmap logoBitmap = BitmapFactory.decodeFile(mLogo.getAbsolutePath());
-        mLocalImage.setImageBitmap(logoBitmap);
+        // CREAZIONE UI REVIEW
+        setUpReviewAct();
 
         if (mBtnReview != null) {
             mBtnReview.setOnClickListener(new View.OnClickListener() {
@@ -64,10 +45,10 @@ public class ReviewActivity extends AppCompatActivity {
                         float userRating = mCurrentRating.getRating();
 
                         // 2) INCREMENTO DI UN VALORE IL NUMERO DELLE RECENSIONI
-                        int updateNumberReviews = reviewInt.getIntExtra("NumberReview",0) + 1;
+                        int updateNumberReviews = mReviewInt.getIntExtra("NumberReview",0) + 1;
 
                         // 3) CALCOLO MEDIA = (VOTO UTENTE + (VOTO TOTALE * NUMERO RECENSIONI) / NUMERO RECENSIONI AGGIORNATO
-                        float updateRatings = (userRating + (reviewInt.getFloatExtra("RatingReview",0.0f) * reviewInt.getIntExtra("NumberReview",0))) / updateNumberReviews;
+                        float updateRatings = (userRating + (mReviewInt.getFloatExtra("RatingReview",0.0f) * mReviewInt.getIntExtra("NumberReview",0))) / updateNumberReviews;
 
                         // PROVA : FACCIO VEDERE IL RISULTATO NELLA RATING BAR CORRENTE (ANCHE SE NON SARA' COSI')
                         mCurrentRating.setRating(updateRatings);
@@ -79,6 +60,34 @@ public class ReviewActivity extends AppCompatActivity {
                     }
                 }
             });
+        }
+    }
+
+    private void setUpReviewAct() {
+        mBtnReview = (Button) findViewById(R.id.buttonReview);
+        mTxtLocalMenu = (TextView) findViewById(R.id.reviewNameLocal);
+        mCurrentRating = (RatingBar) findViewById(R.id.ratingReview);
+        mLocalImage = (ImageView) findViewById(R.id.reviewMenuImage);
+        mTxtLocalMenu.setText(mReviewInt.getStringExtra("LocalName"));
+
+        // CONTROLLO CHAIN PER OTTENERE IL LOGO
+        getChainImage();
+    }
+
+    private void getChainImage() {
+        if(!AppConfiguration.isLogoDownloaded()){
+            mLocalImage.setImageResource(R.drawable.ic_food);
+        }
+        else {
+            if (mTxtLocalMenu.getText().toString().contains(getResources().getString(R.string.mcdonalds))) {
+                mLogo = ScambiaDati.getLogo(0);
+            } else if (mTxtLocalMenu.getText().toString().contains(getResources().getString(R.string.burgerking))) {
+                mLogo = ScambiaDati.getLogo(1);
+            } else if (mTxtLocalMenu.getText().toString().contains(getResources().getString(R.string.baciodilatte))) {
+                mLogo = ScambiaDati.getLogo(2);
+            }
+            Bitmap logoBitmap = BitmapFactory.decodeFile(mLogo.getAbsolutePath());
+            mLocalImage.setImageBitmap(logoBitmap);
         }
     }
 }
