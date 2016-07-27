0550cf332800ea19eada85be87ed782b81f2a649
Corrado <tatinfo94@gmail.com>
2016-07-28 00:58:27 +0200

    DONE : - Inserimento logo in ReviewActivity - Inserimento snackBar in assenza di voto - Uso SharedPreferences per il MultiNotifica
    TO DO :
    - Rivisitazione del layout ReviewActivity

--- app/src/main/java/it/uniba/di/ivu/sms16/gruppo3/fasterfood/notification_screen/ReviewActivity.java
+++ app/src/main/java/it/uniba/di/ivu/sms16/gruppo3/fasterfood/notification_screen/ReviewActivity.java
@@ -1,11 +1,16 @@
 package it.uniba.di.ivu.sms16.gruppo3.fasterfood.notification_screen;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.provider.ContactsContract;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,37 +21,47 @@ import it.uniba.di.ivu.sms16.gruppo3.fasterfood.R;
 import it.uniba.di.ivu.sms16.gruppo3.fasterfood.db.ScambiaDati;
 
 public class ReviewActivity extends AppCompatActivity {
+    private RatingBar mCurrentRating;
+    private Button mBtnReview;
+    private TextView mTxtLocalMenu;
+    private ImageView mLocalImage;
+    private File mLogo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
+
         final Intent reviewInt = getIntent();
 
-        Button btnReview = (Button) findViewById(R.id.buttonReview);
-        TextView txtLocalMenu = (TextView) findViewById(R.id.reviewNameLocal);
-        final RatingBar currentRating = (RatingBar) findViewById(R.id.ratingReview);
-        File logo;
+        mBtnReview = (Button) findViewById(R.id.buttonReview);
+        mTxtLocalMenu = (TextView) findViewById(R.id.reviewNameLocal);
+        mCurrentRating = (RatingBar) findViewById(R.id.ratingReview);
+        mLocalImage = (ImageView) findViewById(R.id.reviewMenuImage);
 
-        txtLocalMenu.setText(reviewInt.getStringExtra("LocalName"));
+
+        mTxtLocalMenu.setText(reviewInt.getStringExtra("LocalName"));
 
         // CONTROLLO CHAIN PER OTTENERE IL LOGO
-        if(txtLocalMenu.getText().toString().contains(getResources().getString(R.string.mcdonalds))){
-            logo = ScambiaDati.getLogo(0);
+        if(mTxtLocalMenu.getText().toString().contains(getResources().getString(R.string.mcdonalds))){
+            mLogo = ScambiaDati.getLogo(0);
         }
-        else if(txtLocalMenu.getText().toString().contains(getResources().getString(R.string.burgerking))){
-            logo = ScambiaDati.getLogo(1);
+        else if(mTxtLocalMenu.getText().toString().contains(getResources().getString(R.string.burgerking))){
+            mLogo = ScambiaDati.getLogo(1);
         }
-        else if(txtLocalMenu.getText().toString().contains(getResources().getString(R.string.baciodilatte))){
-            logo = ScambiaDati.getLogo(2);
+        else if(mTxtLocalMenu.getText().toString().contains(getResources().getString(R.string.baciodilatte))){
+            mLogo = ScambiaDati.getLogo(2);
         }
+        Bitmap logoBitmap = BitmapFactory.decodeFile(mLogo.getAbsolutePath());
+        mLocalImage.setImageBitmap(logoBitmap);
 
-        if (btnReview != null) {
-            btnReview.setOnClickListener(new View.OnClickListener() {
+        if (mBtnReview != null) {
+            mBtnReview.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if(currentRating.getRating() != 0) {
+                    if(mCurrentRating.getRating() != 0) {
                         // 1) PRELEVO IL VALORE DALLA RATING STAR
-                        float userRating = currentRating.getRating();
+                        float userRating = mCurrentRating.getRating();
 
                         // 2) INCREMENTO DI UN VALORE IL NUMERO DELLE RECENSIONI
                         int updateNumberReviews = reviewInt.getIntExtra("NumberReview",0) + 1;
@@ -55,13 +70,12 @@ public class ReviewActivity extends AppCompatActivity {
                         float updateRatings = (userRating + (reviewInt.getFloatExtra("RatingReview",0.0f) * reviewInt.getIntExtra("NumberReview",0))) / updateNumberReviews;
 
                         // PROVA : FACCIO VEDERE IL RISULTATO NELLA RATING BAR CORRENTE (ANCHE SE NON SARA' COSI')
-                        currentRating.setRating(updateRatings);
+                        mCurrentRating.setRating(updateRatings);
 
                         // 4) AGGIORNAMENTO MEDIA E NUMERO DELLE RECENSIONI NEL DB
                     }
                     else {
-                        // PROVA
-                        Toast.makeText(getApplicationContext(),"Non hai votato una sega", Toast.LENGTH_SHORT).show();
+                        Snackbar.make(findViewById(android.R.id.content),"Non hai votato una sega", Snackbar.LENGTH_SHORT).show();
                     }
                 }
             });
