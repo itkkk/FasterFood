39b54ec453affc480a9123d353e2c52ff7ce173e
Corrado <tatinfo94@gmail.com>
2016-07-28 14:32:24 +0200

    - Aggiornamento DB - Miglioramento layout Review Activity

--- app/src/main/java/it/uniba/di/ivu/sms16/gruppo3/fasterfood/notification_screen/ReviewActivity.java
+++ app/src/main/java/it/uniba/di/ivu/sms16/gruppo3/fasterfood/notification_screen/ReviewActivity.java
@@ -3,14 +3,22 @@ package it.uniba.di.ivu.sms16.gruppo3.fasterfood.notification_screen;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 import java.io.File;
 
@@ -20,7 +28,7 @@ import it.uniba.di.ivu.sms16.gruppo3.fasterfood.db.ScambiaDati;
 
 public class ReviewActivity extends AppCompatActivity {
     private Intent mReviewInt;
-    private RatingBar mCurrentRating;
+    private RatingBar mWaitingTimeRating, mLocalServiceRating, mFoodQualityRating;
     private Button mBtnReview;
     private TextView mTxtLocalMenu;
     private ImageView mLocalImage;
@@ -36,27 +44,36 @@ public class ReviewActivity extends AppCompatActivity {
         // CREAZIONE UI REVIEW
         setUpReviewAct();
 
+        // DOVREI CONTROLLARE SE L'UTENTE E' LOGGATO
         if (mBtnReview != null) {
             mBtnReview.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if(mCurrentRating.getRating() != 0) {
+                    if(checkRating()) {
                         // 1) PRELEVO IL VALORE DALLA RATING STAR
-                        float userRating = mCurrentRating.getRating();
+                        float userRatingFQ = mFoodQualityRating.getRating();
+                        float userRatingSL = mLocalServiceRating.getRating();
+                        float userRatingWT = mWaitingTimeRating.getRating();
+
+                        float userRatingTotal = (userRatingFQ + userRatingSL + userRatingWT) / 3;
 
                         // 2) INCREMENTO DI UN VALORE IL NUMERO DELLE RECENSIONI
                         int updateNumberReviews = mReviewInt.getIntExtra("NumberReview",0) + 1;
 
                         // 3) CALCOLO MEDIA = (VOTO UTENTE + (VOTO TOTALE * NUMERO RECENSIONI) / NUMERO RECENSIONI AGGIORNATO
-                        float updateRatings = (userRating + (mReviewInt.getFloatExtra("RatingReview",0.0f) * mReviewInt.getIntExtra("NumberReview",0))) / updateNumberReviews;
+                        float updateRatings = (userRatingTotal + (mReviewInt.getFloatExtra("RatingReview",0.0f) * mReviewInt.getIntExtra("NumberReview",0))) / updateNumberReviews;
 
-                        // PROVA : FACCIO VEDERE IL RISULTATO NELLA RATING BAR CORRENTE (ANCHE SE NON SARA' COSI')
-                        mCurrentRating.setRating(updateRatings);
+                        // PROVA : FACCIO VEDERE IL RISULTATO
+                        System.out.println("Nuova Media : " + updateRatings);
 
                         // 4) AGGIORNAMENTO MEDIA E NUMERO DELLE RECENSIONI NEL DB
+                        changeAverage(getResources().getString(R.string.db_locals),mTxtLocalMenu.getText().toString(), String.valueOf(updateRatings),updateNumberReviews);
+
+                        // 5) RINGRAZIAMENTO ALL'UTENTE
+                        Snackbar.make(findViewById(android.R.id.content),getResources().getText(R.string.review_done), Snackbar.LENGTH_SHORT).show();
                     }
                     else {
-                        Snackbar.make(findViewById(android.R.id.content),"Non hai votato una sega", Snackbar.LENGTH_SHORT).show();
+                        Snackbar.make(findViewById(android.R.id.content),getResources().getText(R.string.review_error), Snackbar.LENGTH_SHORT).show();
                     }
                 }
             });
@@ -64,9 +81,19 @@ public class ReviewActivity extends AppCompatActivity {
     }
 
     private void setUpReviewAct() {
+        // PER OTTENERE LA STATUS BAR DEL COLORE DESIDERATO
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) { 
+            window.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+        }
+
         mBtnReview = (Button) findViewById(R.id.buttonReview);
         mTxtLocalMenu = (TextView) findViewById(R.id.reviewNameLocal);
-        mCurrentRating = (RatingBar) findViewById(R.id.ratingReview);
+        mFoodQualityRating = (RatingBar) findViewById(R.id.ratingFoodQuality);
+        mLocalServiceRating = (RatingBar) findViewById(R.id.ratingLocalService);
+        mWaitingTimeRating = (RatingBar) findViewById(R.id.ratingWaitingTime);
         mLocalImage = (ImageView) findViewById(R.id.reviewMenuImage);
         mTxtLocalMenu.setText(mReviewInt.getStringExtra("LocalName"));
 
@@ -74,6 +101,7 @@ public class ReviewActivity extends AppCompatActivity {
         getChainImage();
     }
 
+    // SE L'APP VIENE CHIUSA FORZATAMENTE E SI CLICCA SULLA NOTIFICA..CRASHA!
     private void getChainImage() {
         if(!AppConfiguration.isLogoDownloaded()){
             mLocalImage.setImageResource(R.drawable.ic_food);
@@ -90,4 +118,33 @@ public class ReviewActivity extends AppCompatActivity {
             mLocalImage.setImageBitmap(logoBitmap);
         }
     }
+
+    private boolean checkRating() {
+        return (mFoodQualityRating.getRating() != 0 && mLocalServiceRating.getRating() != 0 && mWaitingTimeRating.getRating() != 0);
+    }
+
+    public void changeAverage(String DBUrl, final String localName, final String average, final int updateNmbReviews){
+        Firebase ref = new Firebase(DBUrl);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot localSnapshot : dataSnapshot.getChildren()) {
+                    String nome = (String) localSnapshot.child("nome").getValue();
+                    if(nome.equals(localName)){
+                        Firebase localRef = localSnapshot.getRef();
+                        localRef.child("valutazione").setValue(average);
+
+                        String numval = (String) localSnapshot.child("numVal").getValue();
+                        //int nval = (Integer.parseInt(numval))+1;
+                        numval = String.valueOf(updateNmbReviews);
+                        localRef.child("numVal").setValue(numval);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {}
+        });
+    }
+
 }
